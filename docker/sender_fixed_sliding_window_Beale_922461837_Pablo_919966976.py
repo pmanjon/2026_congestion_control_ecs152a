4280492680035a86561582136eedf9f6744ddc50
Pablo Manjon Gamez <pmanjongamez@ucdavis.edu>
2026-02-13 16:17:12 -0800

    latest

--- docker/sender_fixed_sliding_window_Beale_922461837_Pablo_919966976.py
+++ docker/sender_fixed_sliding_window_Beale_922461837_Pablo_919966976.py
@@ -6,10 +6,8 @@ PACKET_SIZE = 1024  # size in bytes of packet
 WINDOW_SIZE = 100   # packets per window
 SEQ_ID_SIZE = 4     # int for the sequence number
 MESSAGE_SIZE = PACKET_SIZE - SEQ_ID_SIZE # 
+port_num = 5001
 
-Throughputs =       []
-totalPacketDelays = [] 
-Performances =      []
 
 def avgTime(someList):
     avgThing = 0.0 
@@ -21,93 +19,129 @@ def avgTime(someList):
 # PlaceHolder for sending the file
 def sendFile():
 
-    FILE = "docker/file.mp3"
+    FILE = "file.mp3"
     with open(FILE, "rb") as file:
         data = file.read()
-    total_packets = int(len(data) / MESSAGE_SIZE)
+    data_length = len(data)
+    total_packets = int(data_length / MESSAGE_SIZE)
     packets_left = total_packets
 
-
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
-        udp_socket.bind(("localhost", 5000))
-        udp_socket.settimeout(1)
+        address, _ = udp_socket.getsockname()
+        udp_socket.connect((address, port_num))
+        udp_socket.settimeout(0.5)
 
         seq_id = 0
-
+        # print("length:", data_length)
         # cur_acks = {}
         # cur_messages = []
-        
-        PacketDelays = {}
+        packetDelays = {}
         window = set()
-        while seq_id < len(data) or len(window) > 0:
+        # We subtract MESSAGE_SIZE to stop the loop just before we send the last packet
+        # this is so we can empty the window first later then send the last packet
+        while seq_id < data_length - MESSAGE_SIZE:
+            print(seq_id)
             if (packets_left == 0): break
-            if (len(window) != WINDOW_SIZE and seq_id < len(data)):
+
+            if (len(window) != WINDOW_SIZE and seq_id + MESSAGE_SIZE < data_length):
                 cur_mes = seq_id.to_bytes(SEQ_ID_SIZE, byteorder='big', signed=True) + data[seq_id: seq_id + MESSAGE_SIZE]
                 udp_socket.sendto(cur_mes, ("localhost", 5001))
 
                 seq_id += MESSAGE_SIZE
                 # print("seq_id:", seq_id)
-                PacketDelays[seq_id] = timer()
+                packetDelays[seq_id] = timer()
                 window.add(seq_id)
 
             else:
                 try:
                     new_data, _ = udp_socket.recvfrom(PACKET_SIZE)
+                    # print("in try")
                     ackHead = int.from_bytes(new_data[:SEQ_ID_SIZE],byteorder='big', signed=True)
+                    # print(window)
+                    print("len", len(window))
+                    print(ackHead)
+                    print(ackHead in window)
                     # cur_acks[ackHead] = True
                     if (ackHead in window):
-                        PacketDelays[ackHead] = timer() - PacketDelays[ackHead]
+                        packetDelays[ackHead] = timer() - packetDelays[ackHead]
                         window.remove(ackHead)
                         # print("removed", ackHead)
                         packets_left -= 1
+                    elif (ackHead >= data_length):
+                        break
 
                 except socket.timeout:
+                    if (seq_id >= data_length):
+                        break
+                    # print("in except")
                     for cur_id in window:
                         actual_id = cur_id - MESSAGE_SIZE
                         new_msg = actual_id.to_bytes(SEQ_ID_SIZE, byteorder='big', signed=True) + data[actual_id: actual_id + MESSAGE_SIZE]
                         udp_socket.sendto(new_msg, ("localhost", 5001))
-                
+                        packetDelays[cur_id] = timer()
+        
+        cleanseWindow(data, udp_socket, window, packetDelays)
+
+        length = min(data_length - seq_id, MESSAGE_SIZE)
+        last_packet = seq_id.to_bytes(SEQ_ID_SIZE, byteorder='big', signed=True) + data[seq_id: seq_id + length]
+
+        udp_socket.sendto(last_packet, ("localhost", 5001))
+        packetDelays[seq_id] = timer()
+        while(True):
+            try:
+                new_data, _ = udp_socket.recvfrom(PACKET_SIZE)
+                ackHead = int.from_bytes(new_data[:SEQ_ID_SIZE],byteorder='big', signed=True)
+                if (ackHead == seq_id + MESSAGE_SIZE): 
+                    packetDelays[seq_id] = timer() - packetDelays[ackHead]
+                    break
+            except socket.timeout:
+                udp_socket.sendto(last_packet, ("localhost", 5001))
+                packetDelays[seq_id] = timer()
+
         # final message 
         final_mess = (-1).to_bytes(SEQ_ID_SIZE, byteorder='big',signed = True) + b"==FINACK=="
         udp_socket.sendto(final_mess, ("localhost", 5001))
-        endPacket = timer()
 
-        not_finished = True
-        while (not_finished):
+    return packetDelays
+
+def cleanseWindow(data, socket: socket.socket, window: set, pack_delays: dict):
+    while (len(window) > 0):
+        cur_id = window.pop()
+        cur_packet = cur_id.to_bytes(SEQ_ID_SIZE, byteorder='big', signed=True) + data[cur_id: cur_id + MESSAGE_SIZE]
+
+        socket.sendto(cur_packet, ("localhost", 5001))
+        pack_delays[cur_id + MESSAGE_SIZE] = timer()
+
+        while(True):
             try:
-                head, _ = udp_socket.recvfrom(PACKET_SIZE)
-                head = int.from_bytes(head[:SEQ_ID_SIZE],byteorder='big', signed=True)
-                PacketDelays[head] = timer() - endPacket
-                not_finished = False
+                new_data, _ = socket.recvfrom(PACKET_SIZE)
+                ackHead = int.from_bytes(new_data[:SEQ_ID_SIZE],byteorder='big', signed=True)
+                if (ackHead == cur_id + MESSAGE_SIZE): 
+                    pack_delays[cur_id + MESSAGE_SIZE] = timer() - pack_delays[ackHead]
+                    break
+                elif (ackHead in window):
+                    pack_delays[ackHead] = timer() - pack_delays[ackHead]
+                    window.remove(ackHead)
             except socket.timeout:
-                udp_socket.sendto(final_mess, ("localhost", 5001))
+                socket.sendto(cur_packet, ("localhost", 5001))
+                pack_delays[cur_id + MESSAGE_SIZE] = timer()
 
-    return PacketDelays
+startThroughput = timer()
 
-sendFile()
-print("end")
-# #sending the same file 10 times to get average
-for x in range(0,10):
-    startThroughput = timer()
-    
-    # replace with actual send file code
-    pack_delays = sendFile()
+# replace with actual send file code
+pack_delays = sendFile()
 
-    # keep this the same 
-    endThroughput = timer()
-    # Throughput = PACKET_SIZE / (endThroughput - startThroughput)
-    Throughput = MESSAGE_SIZE / (endThroughput - startThroughput)
-    Throughputs.append(Throughput)
+# keep this the same 
+endThroughput = timer()
+# Throughput = PACKET_SIZE / (endThroughput - startThroughput)
+Throughput = MESSAGE_SIZE / (endThroughput - startThroughput)
 
-    avgPacketdelay = sum(pack_delays.values()) / len(pack_delays)
-    performance = .3*Throughput + (0.7/avgPacketdelay)
-    Performances.append(performance)
+avgPacketdelay = sum(pack_delays.values()) / len(pack_delays)
+performance = (0.3 / 1000 * Throughput) + (0.7/avgPacketdelay)
 
-    print("trial", x)
-    print("Throughput:", Throughput)
-    print("Avg packet delay:", avgPacketdelay)
-    print("Performance:", performance)
-
+print("Throughput:", Throughput)
+print("Avg packet delay:", avgPacketdelay)
+print("Performance:", performance)
 
 # """
 # Printing the averages of the metrics
